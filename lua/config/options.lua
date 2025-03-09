468f2250e3c3b745a27464e8fafaf3d6b04dfbe9
VimHater <24520826@gm.uit.edu.vn>
2025-03-09 23:34:37 +0700

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,6 +19,58 @@ vim.g.clipboard = {
 vim.opt.clipboard = ""
 
 -- Simple clipboard keymaps
+-- if vim.fn.has("win32") == 1 then
+--   -- Windows: Use built-in clipboard support
+--   vim.opt.clipboard = ""
+-- elseif vim.fn.has("macunix") == 1 then
+--   -- macOS: Use pbcopy and pbpaste
+--   vim.g.clipboard = {
+--     name = "pbcopy",
+--     copy = {
+--       ["+"] = "pbcopy",
+--       ["*"] = "pbcopy",
+--     },
+--     paste = {
+--       ["+"] = "pbpaste",
+--       ["*"] = "pbpaste",
+--     },
+--     cache_enabled = true,
+--   }
+--   vim.opt.clipboard = ""
+-- elseif vim.fn.has("unix") == 1 then
+--   -- Linux: Detect if running Wayland or X11
+--   if os.getenv("WAYLAND_DISPLAY") then
+--     -- Wayland setup
+--     vim.g.clipboard = {
+--       name = "wl-clipboard",
+--       copy = {
+--         ["+"] = "wl-copy",
+--         ["*"] = "wl-copy",
+--       },
+--       paste = {
+--         ["+"] = "wl-paste",
+--         ["*"] = "wl-paste",
+--       },
+--       cache_enabled = true,
+--     }
+--   else
+--     -- X11 setup (xclip or xsel)
+--     vim.g.clipboard = {
+--       name = "xclip",
+--       copy = {
+--         ["+"] = "xclip -selection clipboard",
+--         ["*"] = "xclip -selection primary",
+--       },
+--       paste = {
+--         ["+"] = "xclip -selection clipboard -o",
+--         ["*"] = "xclip -selection primary -o",
+--       },
+--       cache_enabled = true,
+--     }
+--   end
+--   vim.opt.clipboard = ""
+-- end
+
 vim.keymap.set(
   { "n", "v", "i", "c" },
   "<C-S-c>",
@@ -32,3 +84,4 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.g.snacks_animate = false
 vim.g.autoformat = false
+vim.o.linespace = 4
